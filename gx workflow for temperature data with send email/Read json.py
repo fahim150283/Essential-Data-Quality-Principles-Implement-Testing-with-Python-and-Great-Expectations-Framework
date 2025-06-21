fcb830402b6aab27821efe63aca1d59f59d04501
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-06-21 17:30:51 +0600

    the email is not working yet

---
+++ gx workflow for temperature data with send email/Read json.py
@@ -0,0 +1,15 @@
+# this file will read json file and print the needed data
+import json
+
+with open('../Email Credentials.json') as f:
+    data = json.load(f)
+
+mailserveraddr = data['email']['mailServer']
+fromaddr = data['email']['from']
+password = data['email']['password']
+to = data['email']['to']
+
+print (mailserveraddr)
+print (fromaddr)
+print (password)
+print (to)
